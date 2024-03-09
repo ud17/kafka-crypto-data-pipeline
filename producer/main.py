ead69e3cd63d49bf038d0da4094dcdf38e2c5419
Udit Pandya <uditpandya@udits-mbp.home>
2024-03-09 16:37:33 -0500

    init commit

---
+++ producer/main.py
@@ -0,0 +1,36 @@
+# producer.py
+import time
+from kafka import KafkaProducer
+import requests
+import json
+
+def fetch_crypto_prices():
+    # Replace 'your_coin_ids' with the actual coin IDs you want to fetch
+    coin_ids = ['bitcoin', 'ethereum']
+    url = f'https://api.coingecko.com/api/v3/simple/price?ids={"%2C".join(coin_ids)}&vs_currencies=usd'
+    response = requests.get(url)
+    return response.json()
+
+def produce_to_kafka(producer, topic):
+    while True:
+        print('START')
+        crypto_prices = fetch_crypto_prices()
+        # Serialize data to bytes (assuming it's JSON)
+        print('crypto_prices: ' + str(crypto_prices))
+        message_value = json.dumps(crypto_prices).encode('utf-8')
+        producer.send(topic, value=message_value)
+        print('END')
+        time.sleep(5)  # Fetch data every 5 seconds
+
+def main():
+    kafka_bootstrap_servers = 'localhost:9092'
+    kafka_topic = 'crypto_prices'
+    producer = KafkaProducer(bootstrap_servers=kafka_bootstrap_servers)
+
+    try:
+        produce_to_kafka(producer, kafka_topic)
+    except KeyboardInterrupt:
+        producer.close()
+
+if __name__ == "__main__":
+    main()
